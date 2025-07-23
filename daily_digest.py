027db589be2c9f1dd458ddda2b9814ad136e2ba4
Haus226 <haus0525@gmail.com>
2025-07-23 23:06:08 +0800

    Fix logging bug

--- daily_digest.py
+++ daily_digest.py
@@ -203,7 +203,6 @@ def demo():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, 
-                        filename=".log",
                         format="%(levelname)s   %(asctime)s [%(filename)s:%(lineno)d:%(funcName)s:%(threadName)s] %(message)s")
     content = run()
     send_email("📅 Daily Digest", content)
